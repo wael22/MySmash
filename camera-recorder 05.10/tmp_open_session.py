fd064a1c88f867a12e8da1dd5ccdd200c4a8346f
Wael Nouira <nouira.home@gmail.com>
2025-12-10 23:52:45 +0100

    feat: Add complete frontend and backend source code

--- camera-recorder 05.10/tmp_open_session.py
+++
@@ -1,4 +0,0 @@
-import requests
-r = requests.post('http://127.0.0.1:8000/session/open', json={'url':'http://212.231.225.55:88/axis-cgi/mjpg/video.cgi'}, timeout=10)
-print(r.status_code)
-print(r.text)
--- camera-recorder 05.10/tmp_start_record.py
+++
@@ -1,4 +0,0 @@
-import requests
-r = requests.post('http://127.0.0.1:8000/record/start', json={'session_id':'2d4efdfe-0fe8-40cc-8991-da3a1aa55603','duration_seconds':10})
-print(r.status_code)
-print(r.text)
