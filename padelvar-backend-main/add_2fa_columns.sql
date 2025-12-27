b7815eb09bbf8586e66b0aff397d63f8c8e1fe7f
Wael Nouira <nouira.home@gmail.com>
2025-12-27 23:40:49 +0100

    chore: Production cleanup - remove 63 unused files

--- padelvar-backend-main/add_2fa_columns.sql
+++
@@ -1,6 +0,0 @@
--- Script SQL pour ajouter les colonnes 2FA à la table user
--- À exécuter manuellement pour résoudre le problème de migration
-
-ALTER TABLE user ADD COLUMN two_factor_secret VARCHAR(255);
-ALTER TABLE user ADD COLUMN two_factor_enabled BOOLEAN DEFAULT 0;
-ALTER TABLE user ADD COLUMN two_factor_backup_codes TEXT;
